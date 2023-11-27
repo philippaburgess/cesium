8438a66ea746f2007adf87fb6b2e74f30fce0c3a
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2023-11-26 19:28:41 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Set your Cesium Ion default access token.
-  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODMwMDg5ZC0wYjJlLTQ2NmEtOTg5Ny1iMzI0NzNjMjU5YjYiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTk4NTI5NDh9.VtT7XV6WVveRJijzrNyZLOsooZ6p14yChusoetLIL54';
+  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODMwMDg5ZC0wYjJlLTQ2NmEtOTg5Ny1iMzI0NzNjMjU5YjYiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTk4NTI5NDh9.VtT7XV6WVveRJijzrNyZLOsooZ6p14yChusoetLIL54eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODMwMDg5ZC0wYjJlLTQ2NmEtOTg5Ny1iMzI0NzNjMjU5YjYiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTk4NTI5NDh9.VtT7XV6WVveRJijzrNyZLOsooZ6p14yChusoetLIL54';
 
   const viewer = new Cesium.Viewer('cesiumContainer', {
     imageryProvider: new Cesium.IonImageryProvider({ assetId: 3954 }), // Replace with the actual asset ID for the imagery layer
@@ -17,9 +17,10 @@ document.addEventListener('DOMContentLoaded', function() {
     animation: false
   });
 
-  // Load the photorealistic 3D Tiles. You have to replace assetId with the correct one.
+  
+  // Load the photorealistic 3D Tiles. 
   var tileset = new Cesium.Cesium3DTileset({
-    url: Cesium.IonResource.fromAssetId(2275207) // Replace with the actual asset ID for the 3D Tiles
+    url: Cesium.IonResource.fromAssetId(2275207)
   });
 
   viewer.scene.primitives.add(tileset);
@@ -30,18 +31,16 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log(error);
   });
 
-  // Fly to Port of Long Beach
+
   viewer.camera.flyTo({
     destination: Cesium.Cartesian3.fromDegrees(-118.2153, 33.7550, 1500),
     orientation: {
       heading: Cesium.Math.toRadians(0),
-      pitch: Cesium.Math.toRadians(-30),
+      pitch: Cesium.Math.toRadians(-35),
       roll: 0.0
     }
   });
 });
-
-
 
 
 
