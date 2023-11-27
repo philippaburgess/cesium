69b102112c4d16e2a37253ad571a08f721b17fe9
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2023-11-27 10:34:59 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,37 @@
 // Grant CesiumJS access to your ion assets
-Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmZTE0OGI1Ni03MTk3LTQxYzYtYTc3MS1mY2M1ZDFjMzYyYmMiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTgwODUwNDF9.VeJhEdO20c8chc58WWuT3VGj5wiuSLdHqx0I4S7mfss";
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODMwMDg5ZC0wYjJlLTQ2NmEtOTg5Ny1iMzI0NzNjMjU5YjYiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTk4NTI5NDh9.VtT7XV6WVveRJijzrNyZLOsooZ6p14yChusoetLIL54";
 
 const viewer = new Cesium.Viewer("cesiumContainer", {
-  // This is a global 3D Tiles tileset so disable the
-  // globe to prevent it from interfering with the data
-  globe: false,
-  // Disabling the globe means we need to manually
-  // re-enable the atmosphere
-  skyAtmosphere: new Cesium.SkyAtmosphere(),
-  // 2D and Columbus View are not currently supported
-  // for global 3D Tiles tilesets
-  sceneModePicker: false,
-  // Imagery layers are not currently supported for
-  // global 3D Tiles tilesets
-  baseLayerPicker: false,
+  // Use Cesium World Terrain
+  terrainProvider: Cesium.createWorldTerrain(),
+  // Improve performance by not automatically requesting render
+  requestRenderMode: true,
+  // Reduce the maximum number of terrain tiles to load (for performance)
+  maximumScreenSpaceError: 16,
 });
 
-try {
-  const tileset = await Cesium.Cesium3DTileset.fromIonAssetId(2275207);
-  viewer.scene.primitives.add(tileset);
-} catch (error) {
-  console.log(error);
+// Adding the 3D Tiles Photorealistic layer
+viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
+  url: Cesium.IonResource.fromAssetId(2275207)
+}));
 
-
-    // Set the camera to view the Port of Long Beach
-    viewer.camera.setView({
-      destination: Cesium.Cartesian3.fromDegrees(-118.2153, 33.7550, 1000), // Replace with the desired altitude
-      orientation: {
-        heading: Cesium.Math.toRadians(0),
-        pitch: Cesium.Math.toRadians(-35), // Tilt the view down
-        roll: 0.0
-      }
-    });
-  }).catch(function(error) {
-    console.error(error);
-  });
+// Fly the camera to the Port of Long Beach
+viewer.camera.flyTo({
+  destination: Cesium.Cartesian3.fromDegrees(-118.2153, 33.7550, 1000),
+  orientation: {
+    heading: Cesium.Math.toRadians(0),
+    pitch: Cesium.Math.toRadians(-35),
+    roll: 0.0
+  }
 });
 
-var featureLayer = new Cesium.ArcGisFeatureServer({
-  url: 'https://services1.arcgis.com/ZIL9uO234SBBPGL7/arcgis/rest/services/Health_Impact_Points_POLB_OptimizedD2CANCER/SceneServer'
-});
+// Adding the ArcGIS Web Feature Layer as GeoJSON
+const featureLayerUrl = 'https://services1.arcgis.com/ZIL9uO234SBBPGL7/arcgis/rest/services/Health_Impact_Points_POLB_OptimizedD2CANCER/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=geojson';
 
-viewer.dataSources.add(featureLayer);
+viewer.dataSources.add(Cesium.GeoJsonDataSource.load(featureLayerUrl, {
+  // Set the marker symbol and color here if needed
+  // Example: pointToLayer and onEachFeature functions
+}));
+
 
 
