c5e118845d92d1cf957f7ba50f8d88b70a94f34c
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2023-11-27 12:39:57 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
   // Set your Cesium Ion default access token.
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODMwMDg5ZC0wYjJlLTQ2NmEtOTg5Ny1iMzI0NzNjMjU5YjYiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTk4NTI5NDh9.VtT7XV6WVveRJijzrNyZLOsooZ6p14yChusoetLIL54';
 
+ry {
+  const tileset = await Cesium.createGooglePhotorealistic3DTileset();
+  viewer.scene.primitives.add(tileset);
+} catch (error) {
+  console.log(`Failed to load tileset: ${error}`);
+}
+  
   // Create the Cesium Viewer with the specified imagery provider and terrain provider.
   const viewer = new Cesium.Viewer('cesiumContainer', {
     imageryProvider: new Cesium.IonImageryProvider({ assetId: 2275207 }), // Use your Google Photorealistic 3D Tiles asset ID
@@ -19,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Fly the camera to the Port of Long Beach.
   viewer.camera.flyTo({
-    destination: Cesium.Cartesian3.fromDegrees(-118.2153, 33.7550, 1500),
+    destination: Cesium.Cartesian3.fromDegrees(-118.2153, 33.7550, 1000),
     orientation: {
       heading: Cesium.Math.toRadians(0),
       pitch: Cesium.Math.toRadians(-30),
