aa8bba6dd23a75d5e3722eae98c78f0f0ee88775
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2023-11-26 18:44:14 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
   // Set your Cesium Ion default access token.
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODMwMDg5ZC0wYjJlLTQ2NmEtOTg5Ny1iMzI0NzNjMjU5YjYiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTk4NTI5NDh9.VtT7XV6WVveRJijzrNyZLOsooZ6p14yChusoetLIL54';
 
+const viewer = new Cesium.Viewer("cesiumContainer", {
+    globe: false,
+});
+  
   const viewer = new Cesium.Viewer('cesiumContainer', {
     imageryProvider: new Cesium.IonImageryProvider({ assetId: 2275207 }),
     terrainProvider: Cesium.createWorldTerrain(),
