b0c77f2a6b40564808fce9040cdb32aac5c030ae
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2023-11-27 11:42:58 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-import { Cartesian3, createOsmBuildingsAsync, Ion, Math as CesiumMath, Terrain, Viewer } from 'cesium';
-
 // Grant CesiumJS access to your ion assets
-document.addEventListener('DOMContentLoaded', function() {
+
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODMwMDg5ZC0wYjJlLTQ2NmEtOTg5Ny1iMzI0NzNjMjU5YjYiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTk4NTI5NDh9.VtT7XV6WVveRJijzrNyZLOsooZ6p14yChusoetLIL54";
- 
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Create a new viewer with the terrain provider and disable unwanted widgets
   const viewer = new Cesium.Viewer('cesiumContainer', {
     terrainProvider: Cesium.createWorldTerrain(),
-
-  
-const viewer = new Cesium.Viewer('cesiumContainer', {
-    imageryProvider: new Cesium.IonImageryProvider({
-      assetId: 2275207  // Asset ID for the photorealistic 3D Tiles
-    }),
-    terrainProvider: new Cesium.CesiumTerrainProvider({
-      url: Cesium.IonResource.fromAssetId(1)  // Cesium World Terrain
-    }),
     baseLayerPicker: false,
     geocoder: false,
     homeButton: false,
@@ -23,44 +14,35 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
     sceneModePicker: false,
     timeline: false,
     animation: false,
-    requestRenderMode: true
+    requestRenderMode: true,
+    globe: false, // Disable the globe
+    skyAtmosphere: new Cesium.SkyAtmosphere(), // Manually enable the atmosphere
   });
 
-  const tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
-    url: Cesium.IonResource.fromAssetId(2275207)  // Again, the Asset ID for the photorealistic 3D Tiles
-  }));
-
-  tileset.readyPromise.then(function(tileset) {
-    viewer.zoomTo(tileset);
-
-   
-try {
-  const tileset = await Cesium.createGooglePhotorealistic3DTileset();
-  viewer.scene.primitives.add(tileset);
-} catch (error) {
-  console.log(`Failed to load tileset: ${error}`);
-}
-
-  });
-
+  // Load the photorealistic 3D Tiles and add it to the scene
   const tileset = new Cesium.Cesium3DTileset({
-    url: Cesium.IonResource.fromAssetId(2275207) // Asset ID for the photorealistic 3D Tiles
+    url: Cesium.IonResource.fromAssetId(2275207) // Use the correct Asset ID for the photorealistic 3D Tiles
   });
 
-const viewer = new Cesium.Viewer("cesiumContainer", {
-    globe: false,
-});
- 
-  viewer.scene.globe.enableLighting = true;
+  viewer.scene.primitives.add(tileset);
 
-  viewer.camera.flyTo({
-    destination: Cesium.Cartesian3.fromDegrees(-118.2153, 33.7550, 1000),
-    orientation: {
-      heading: Cesium.Math.toRadians(0),
-      pitch: Cesium.Math.toRadians(-35),
-      roll: 0.0
-    },
-    duration: 3
+  tileset.readyPromise.then(function(loadedTileset) {
+    // Zoom to the tileset once it's ready
+    viewer.zoomTo(loadedTileset);
+
+    // Set the camera to view the Port of Long Beach
+    viewer.camera.setView({
+      destination: Cesium.Cartesian3.fromDegrees(-118.2153, 33.7550, 1000),
+      orientation: {
+        heading: Cesium.Math.toRadians(0),
+        pitch: Cesium.Math.toRadians(-35), // Tilt the view down
+        roll: 0.0
+      }
+    });
+  }).catch(function(error) {
+    console.error("Error loading Google Photorealistic 3D Tiles:", error);
   });
 });
+
+
 
