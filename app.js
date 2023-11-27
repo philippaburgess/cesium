cee3242a5493d8060d776d2e31ff759f4d176601
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2023-11-26 20:16:46 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   // Set your Cesium Ion default access token.
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODMwMDg5ZC0wYjJlLTQ2NmEtOTg5Ny1iMzI0NzNjMjU5YjYiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTk4NTI5NDh9.VtT7XV6WVveRJijzrNyZLOsooZ6p14yChusoetLIL54';
 
+  // Create a new Cesium Viewer with the default imagery and terrain
   const viewer = new Cesium.Viewer('cesiumContainer', {
-    imageryProvider: new Cesium.IonImageryProvider({ assetId: 2275207 }), // Photorealistic 3D tiles
-    terrainProvider: Cesium.createWorldTerrain(), // This uses Cesium's global terrain by default
     baseLayerPicker: false,
     geocoder: false,
     homeButton: false,
@@ -12,16 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
     navigationHelpButton: false,
     sceneModePicker: false,
     timeline: false,
-    animation: false,
+    animation: false
   });
 
-  // Fly to Port of Long Beach
+  // Fly to Port of Long Beach with a duration of 3 seconds to ensure you can see the transition
   viewer.camera.flyTo({
     destination: Cesium.Cartesian3.fromDegrees(-118.2153, 33.7550, 1000),
     orientation: {
       heading: Cesium.Math.toRadians(0),
       pitch: Cesium.Math.toRadians(-35),
       roll: 0.0
-    }
+    },
+    duration: 3
   });
 });
