4acd7a122f14787c5d4cb96c3f373f291e69c11e
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2023-11-26 21:22:59 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Set your Cesium Ion default access token.
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODMwMDg5ZC0wYjJlLTQ2NmEtOTg5Ny1iMzI0NzNjMjU5YjYiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTk4NTI5NDh9.VtT7XV6WVveRJijzrNyZLOsooZ6p14yChusoetLIL54';
-  const viewer = new Cesium.Viewer('cesiumContainer', {
+  
+onst viewer = new Cesium.Viewer('cesiumContainer', {
+    imageryProvider: new Cesium.IonImageryProvider({
+      assetId: 2275207  // Asset ID for the photorealistic 3D Tiles
+    }),
+    terrainProvider: new Cesium.CesiumTerrainProvider({
+      url: Cesium.IonResource.fromAssetId(1)  // Cesium World Terrain
+    }),
     baseLayerPicker: false,
+    geocoder: false,
     homeButton: false,
     infoBox: false,
     navigationHelpButton: false,
     sceneModePicker: false,
     timeline: false,
     animation: false,
-    requestRenderMode: true,
+    requestRenderMode: true
   });
 
-  const imageryLayer = viewer.imageryLayers.addImageryProvider(new Cesium.IonImageryProvider({
-    assetId: 2275207 // The ID for the photorealistic 3D Tiles
-  }));
-  
   const tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
-    url: Cesium.IonResource.fromAssetId(2275207) // The ID for the photorealistic 3D Tiles
+    url: Cesium.IonResource.fromAssetId(2275207)  // Again, the Asset ID for the photorealistic 3D Tiles
   }));
 
-  tileset.readyPromise.then(function() {
+  tileset.readyPromise.then(function(tileset) {
     viewer.zoomTo(tileset);
+    // Set the camera to view the Port of Long Beach
+    viewer.camera.setView({
+      destination: Cesium.Cartesian3.fromDegrees(-118.2153, 33.7550, 1000), // Replace with the desired altitude
+      orientation: {
+        heading: Cesium.Math.toRadians(0),
+        pitch: Cesium.Math.toRadians(-35), // Tilt the view down
+        roll: 0.0
+      }
+    });
   }).catch(function(error) {
     console.error(error);
-  });
-
-  viewer.camera.flyTo({
-    destination: Cesium.Cartesian3.fromDegrees(-118.2153, 33.7550, 1500),
-    orientation: {
-      heading: Cesium.Math.toRadians(0),
-      pitch: Cesium.Math.toRadians(-35),
-      roll: 0.0
-    }
   });
 });
