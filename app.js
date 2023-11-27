1cb29c8292ff8367b50a18e4f1ee6960cd3b384e
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2023-11-26 19:42:08 -0800

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODMwMDg5ZC0wYjJlLTQ2NmEtOTg5Ny1iMzI0NzNjMjU5YjYiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTk4NTI5NDh9.VtT7XV6WVveRJijzrNyZLOsooZ6p14yChusoetLIL54';
 
   const viewer = new Cesium.Viewer('cesiumContainer', {
-    imageryProvider: new Cesium.IonImageryProvider({ assetId: 3954 }), // Replace with the actual asset ID for the imagery layer
-    terrainProvider: new Cesium.CesiumTerrainProvider({
-      url: Cesium.IonResource.fromAssetId(1)
-    }),
+    imageryProvider: new Cesium.IonImageryProvider({ assetId: 2275207 }), // Photorealistic 3D tiles
+    terrainProvider: Cesium.createWorldTerrain(), // This uses Cesium's global terrain by default
     baseLayerPicker: false,
     geocoder: false,
     homeButton: false,
@@ -17,21 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
     animation: false
   });
 
-  
-  // Load the photorealistic 3D Tiles. 
-  var tileset = new Cesium.Cesium3DTileset({
-    url: Cesium.IonResource.fromAssetId(2275207)
-  });
-
-  viewer.scene.primitives.add(tileset);
-
-  tileset.readyPromise.then(function() {
-    viewer.zoomTo(tileset);
-  }).otherwise(function (error) {
-    console.log(error);
-  });
-
-
+  // Fly to Port of Long Beach
   viewer.camera.flyTo({
     destination: Cesium.Cartesian3.fromDegrees(-118.2153, 33.7550, 1500),
     orientation: {
@@ -42,6 +26,5 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-
-
+  
 
