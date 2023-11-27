46d4afde3777925a866d5b2a9e317dd7b38a141d
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2023-11-26 19:54:15 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
   // Set your Cesium Ion default access token.
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ODMwMDg5ZC0wYjJlLTQ2NmEtOTg5Ny1iMzI0NzNjMjU5YjYiLCJpZCI6MTczNDE4LCJpYXQiOjE2OTk4NTI5NDh9.VtT7XV6WVveRJijzrNyZLOsooZ6p14yChusoetLIL54';
 
-    // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-    const viewer = new Cesium.Viewer('cesiumContainer', {
-      terrain: Cesium.Terrain.fromWorldTerrain(),
-    });    
+  const viewer = new Cesium.Viewer('cesiumContainer', {
+    imageryProvider: new Cesium.IonImageryProvider({ assetId: 2275207 }), // Photorealistic 3D tiles
+    terrainProvider: Cesium.createWorldTerrain(), // This uses Cesium's global terrain by default
+    baseLayerPicker: false,
+    geocoder: false,
+    homeButton: false,
+    infoBox: false,
+    navigationHelpButton: false,
+    sceneModePicker: false,
+    timeline: false,
+    animation: false
+  });
 
-    // Fly the camera to San Francisco at the given longitude, latitude, and height.
-    viewer.camera.flyTo({
-      destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-      orientation: {
-        heading: Cesium.Math.toRadians(0.0),
-        pitch: Cesium.Math.toRadians(-15.0),
-      }
-    });
-
-  
+  // Fly to Port of Long Beach
+  viewer.camera.flyTo({
+    destination: Cesium.Cartesian3.fromDegrees(118.2153, 33.7550, 1500),
+    orientation: {
+      heading: Cesium.Math.toRadians(0),
+      pitch: Cesium.Math.toRadians(-35),
+      roll: 0.0
+    }
+  });
+});
